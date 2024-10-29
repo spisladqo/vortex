b8576b70f6fde0df68d8cb683791c817bf6f01ef
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2024-10-29 22:30:15 +0300

    Fix order of loops in script for running tests

--- custom_kernels.sh
+++ custom_kernels.sh
@@ -6,9 +6,9 @@ END_KERNEL_NUM=4
 
 rm -rf $OUTPUT_FILE
 for ((i=START_KERNEL_NUM;i<=END_KERNEL_NUM; i++)); do
-	for driver in "rtlsim" "simx"; do
-		for arg in "-n32" "-n128"; do
-			for perf in "1" "2"; do
+	for arg in "-n32" "-n128"; do
+		for perf in "1" "2"; do
+			for driver in "rtlsim" "simx"; do
 				echo;
 				echo "Kernel$i, $driver, perf=$perf, args=$arg" | tee -a $OUTPUT_FILE;
 				./ci/blackbox.sh --app=kernel$i --driver=$driver --perf=$perf --args=$arg >> $OUTPUT_FILE
